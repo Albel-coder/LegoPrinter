a8c0a0fb7ddd8ecde2a7a73bf771efe2bb241510
Aleksey <alekseybel2505@gmail.com>
2025-11-30 23:52:00 +0300

    Updated Iprinter interface

--- Include/IPrinter.h
+++ Include/IPrinter.h
@@ -1,48 +1,70 @@
-#pragma once
+﻿#pragma once
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// opaque pointer
-typedef struct IPrinter IPrinter;
+    typedef struct IPrinter IPrinter;
 
-// struct for motor config
-typedef struct
-{
-	unsigned char Port;
-	signed char Speed;
-	double Revolutions;
-} MotorCommand;
+    typedef struct
+    {
+        unsigned char port;
+        signed char speed;
+        double revolutions;
+    } MotorCommand;
 
-// Virtual table of printer methods
-typedef struct
-{
-	// Main operations
-	bool (*Connect)(IPrinter* Self);
-	bool (*Disconnect)(IPrinter* Self);
-	bool (*IsConnected)(IPrinter* Self);
-	void (*Destroy)(IPrinter* Self);
+    typedef struct
+    {
+        double distance; // Target position in revolutions
+        signed char speed; // Speed for current position (-100 to 100)
+        double tolerance; // Tolerance for position
+    } SpeedProfilePoint;
 
-	// Motors control
-	void (*RotateMotor)(IPrinter* Self, const MotorCommand* Commands, int Count);
+    typedef struct
+    {
+        unsigned char port;
+        SpeedProfilePoint* points;
+        int count;
+        int timeoutMs; // Timeout for all profile
+    } SpeedProfile;
 
-	// Raw command
-	void (*SendCommand)(IPrinter* Self, const unsigned char* Command, int Length);
+    // Virtual table - ONLY low-level methods
+    typedef struct 
+    {
+        // Basic Operations
+        bool (*printer_connect)(IPrinter* self);
+        bool (*printer_disconnect)(IPrinter* self);
+        bool (*printer_is_connected)(IPrinter* self);
+        void (*printer_destroy)(IPrinter* self);
 
-	int (*GetLogCount)(IPrinter* Self);                       
-	const char* (*GetLogEntry)(IPrinter* Self, int index);   
-	void (*ClearLog)(IPrinter* Self);                         
-	const char* (*GetLastError)(IPrinter* Self);              
-	void (*PrintConnectionInfo)(IPrinter* Self);              
+        void (*printer_rotate_motor)(IPrinter* self, const MotorCommand* commands, int count);
 
-} IPrinterVirtualTable;
+        void (*printer_set_motor_speed)(IPrinter* self, unsigned char port, signed char speed);
 
-// Main struct interface
-struct IPrinter
-{
-	IPrinterVirtualTable* VirtualTable;
-};
+        // Raw command
+        void (*printer_send_command)(IPrinter* self, const unsigned char* command, int length);
+
+        bool (*printer_printer_execute_speed_profile)(IPrinter* self, const SpeedProfile* profile);
+
+        // Monitoring
+        bool (*printer_is_motor_moving)(IPrinter* self, unsigned char port);
+        double (*printer_get_motor_position)(IPrinter* self, unsigned char port);
+
+        // Logging
+        int (*printer_get_log_count)(IPrinter* self);
+        const char* (*printer_get_log_entry)(IPrinter* self, int index);
+        void (*printer_clear_log)(IPrinter* self);
+        const char* (*printer_get_last_error)(IPrinter* self);
+        void (*printer_printer_connection_info)(IPrinter* self);
+
+        bool (*printer_test_encoder_functionality)(IPrinter* self);
+
+    } IPrinterVirtualTable;
+
+    struct IPrinter 
+    {
+        IPrinterVirtualTable* vtable;
+    };    
 
 #ifdef __cplusplus
 }
