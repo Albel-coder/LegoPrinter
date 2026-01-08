5c514da3edac6fa258ffb27338d65df586699215
Aleksey <alekseybel2505@gmail.com>
2026-01-08 22:32:01 +0300

    start working with android project

---
+++ Android/app/src/main/java/com/example/lpstudio/PrinterController.java
@@ -0,0 +1,20 @@
+package com.example.lpstudio;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Java wrapper for C++ printer driver
+// C# PrinterController Analog
+public class PrinterController implements AutoCloseable {
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+
+    }
+}
