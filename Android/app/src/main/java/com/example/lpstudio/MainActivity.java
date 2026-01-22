a384b3d852a1803d4c7709b64169b82664b7c632
Aleksey <alekseybel2505@gmail.com>
2026-01-22 23:14:40 +0300

    fixed: main fragments switch logic

--- Android/app/src/main/java/com/example/lpstudio/MainActivity.java
+++ Android/app/src/main/java/com/example/lpstudio/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.lpstudio;
 
+import com.example.lpstudio.DeviceFragment;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -36,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         buttonCalibration = findViewById(R.id.buttonCalibration);
 
         // Initializing tabs
-        setupTabs();
+        tabs.put("DeviceUserControl", new DeviceFragment());
 
         // Assigning click handlers
         buttonDevice.setOnClickListener(new View.OnClickListener() {
@@ -71,22 +73,8 @@ public class MainActivity extends AppCompatActivity {
         showTab("DeviceUserControl");
     }
 
-    private void setupTabs() {
-        // Here we will create instances of Kotlin fragments
-        tabs.put("DeviceUserControl", createPlaceholderFragment("Device Tab Content", R.color.teal_200));
-        tabs.put("PreviewUserControl", createPlaceholderFragment("Preview Tab Content", R.color.purple_500));
-        tabs.put("PrepareUserControl", createPlaceholderFragment("Prepare Tab Content", R.color.teal_700));
-        tabs.put("CalibrationUserControl", createPlaceholderFragment("Calibration Tab Content", R.color. purple_700));
-    }
 
-    private Fragment createPlaceholderFragment(String text, int colorResId) {
-        PlaceholderFragment fragment = new PlaceholderFragment();
-        Bundle args = new Bundle();
-        args.putString("text", text);
-        args.putInt("color", colorResId);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     private void showTab(String tabKey) {
         Fragment fragment = tabs.get(tabKey);
@@ -98,20 +86,22 @@ public class MainActivity extends AppCompatActivity {
         // We begin the transaction of fragments
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
-        // If the fragment has not been added yet, replace the current one
-        if (currentFragment != null) {
-            transaction.hide(currentFragment);
-        }
-
-        // If the fragment has already been added, just show it
-        if (!fragment.isAdded()) {
+        // If this is the first time showing a fragment
+        if (currentFragment == null) {
             transaction.add(R.id.contentContainer, fragment, tabKey);
         }
-        else {
-            transaction.show(fragment);
+        // If switching between fragments
+        else if (currentFragment != fragment) {
+            // If fragment was already added, just show it
+            if (fragment.isAdded()) {
+                transaction.hide(currentFragment).show(fragment);
+            }
+            // If fragment hasn`t been added yet
+            else {
+                transaction.hide(currentFragment).add(R.id.contentContainer, fragment, tabKey);
+            }
         }
 
-        transaction.commit();
         currentFragment = fragment;
     }
 
@@ -123,81 +113,72 @@ public class MainActivity extends AppCompatActivity {
         setButtonSelected(buttonCalibration, false);
 
         // Select the selected button
-        switch(selectedTab) {
-            case "DeviceUserControl":
-                setButtonSelected(buttonDevice, true);
-                break;
-            case "PreviewUserControl":
-                setButtonSelected(buttonPreview, true);
-                break;
-            case "PrepareUserControl":
-                setButtonSelected(buttonPrepare, true);
-                break;
-            case "CalibrationUserControl":
-                setButtonSelected(buttonCalibration, true);
-                break;
+        if ("DeviceUserControl".equals(selectedTab)) {
+            setButtonSelected(buttonDevice, true);
+        }
+        else if ("PreviewUserControl".equals(selectedTab)) {
+            setButtonSelected(buttonPreview, true);
+        }
+        else if ("PrepareUserControl".equals(selectedTab)) {
+            setButtonSelected(buttonPrepare, true);
+        }
+        else if ("CalibrationUserControl".equals(selectedTab)) {
+            setButtonSelected(buttonCalibration, true);
         }
     }
 
     private void setButtonSelected(Button button, boolean selected) {
+        button.setSelected(selected);
 
-        // Change the button color depending on its state
-        ImageView icon = null;
-        TextView text = null;
-
-        // Finding the child elements of a button
-        for (int i = 0; i < ((android.view.ViewGroup) button).getChildCount(); i++) {
-            View child = ((android.view.ViewGroup) button).getChildAt(i);
-            if (child instanceof ImageView) {
-                icon = (ImageView) child;
-            }
-            else if (child instanceof TextView) {
-                text = (TextView) child;
-            }
+        // Change the text color depending on the state
+        if (selected) {
+            button.setTextColor(ContextCompat.getColor(this, R.color.bottom_nav_selected));
+        }
+        else {
+            button.setTextColor(ContextCompat.getColor(this, R.color.bottom_nav_unselected));
         }
 
-        if(selected) {
-            // Selected state
-            if (icon != null) {
-                // Change the icon to a filled version
-                switch (button.getId()) {
-                    case R.id.buttonDevice:
-                        icon.setImageResource(R.drawable.ic_device_filled);
-                        break;
-                    case R.id.buttonPreview:
-                        icon.setImageResource(R.drawable.ic_preview_filled);
-                        break;
-                    case R.id.buttonPrepare:
-                        icon.setImageResource(R.drawable.ic_prepare_filled);
-                        break;
-                    case R.id.buttonCalibration:
-                        icon.setImageResource(R.drawable.ic_calibration_filled);
-                        break;
-                }
+        // Change the icon depending on the state and ID of the button
+        int iconResId = 0;
+        int buttonId = button.getId();
+
+        // Use if-else instead of switch (to avoid the "Constant expression required" error)
+        if (selected) {
+            // Selected state - filled icons
+            if (buttonId == R.id.buttonDevice) {
+                iconResId = R.drawable.ic_device_filled;
             }
-            if (text != null) {
-                text.setTextColor(ContextCompat.getColor(this, R.color.bottom_nav_selected));
+            else if (buttonId == R.id.buttonPreview) {
+                iconResId = R.drawable.ic_preview_filled;
+            }
+            else if (buttonId == R.id.buttonPrepare) {
+                iconResId = R.drawable.ic_prepare_filled;
+            }
+            else if (buttonId == R.id.buttonCalibration) {
+                iconResId = R.drawable.ic_calibration_filled;
             }
         }
         else {
-            // Not selected state
-            if (icon != null) {
-                // Change the icon to the outline version
-                switch(button.getId()) {
-                    case R.id.buttonDevice:
-                        icon.setImageResource(R.drawable.ic_device_outline);
-                        break;
-                    case R.id.buttonPreview:
-                        icon.setImageResource(R.drawable.ic_preview_outline);
-                        break;
-                    case R.id.buttonPrepare:
-                        icon.setImageResource(R.drawable.ic_prepare_outline);
-                        break;
-                    case R.id.buttonCalibration:
-                        icon.setImageResource(R.drawable.ic_calibration_outline);
-                        break;
-                }
+            // Unselected state - outline icons
+            if (buttonId == R.id.buttonDevice) {
+                iconResId = R.drawable.ic_device_outline;
             }
+            else if (buttonId == R.id.buttonPreview) {
+                iconResId = R.drawable.ic_preview_outline;
+            }
+            else if (buttonId == R.id.buttonPrepare) {
+                iconResId = R.drawable.ic_prepare_outline;
+            }
+            else if (buttonId == R.id.buttonCalibration) {
+                iconResId = R.drawable.ic_calibration_outline;
+            }
+        }
+
+        // Installing the icon
+        if (iconResId != 0) {
+            Drawable icon = ContextCompat.getDrawable(this, iconResId);
+            // Place the icon above the text
+            button.setCompoundDrawablesWithIntrinsicBounds(null, icon, null, null);
         }
     }
 }
---
+++ Android/app/src/main/java/com/example/lpstudio/PlaceholderFragment.java
@@ -0,0 +1,36 @@
+package com.example.lpstudio;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class PlaceholderFragment extends Fragment {
+
+    public PlaceholderFragment() {
+        // Required empty constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_placeholder, container, false);
+
+        TextView textView = view.findViewById(R.id.textView);
+        View background = view.findViewById(R.id.background);
+
+        if (getArguments() != null) {
+            String text = getArguments().getString("text");
+            int colorResId = getArguments().getInt("color", R.color.teal_200);
+
+            textView.setText(text);
+            background.setBackgroundColor(getResources().getColor(colorResId));
+        }
+
+        return view;
+    }
+}
