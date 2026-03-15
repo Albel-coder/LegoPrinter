48f7fca702b52fa963c26a6899cedc5217a1acb8
Aleksey <alekseybel2505@gmail.com>
2026-03-15 23:30:31 +0300

    refactor transport interface for major update

--- PrinterDriver/src/core/driver/interfaces/ITransport.h
+++ PrinterDriver/src/core/driver/interfaces/ITransport.h
@@ -1,25 +1,62 @@
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
 #include <functional>
-#include <vector>
+#include <map>
 #include <memory>
+#include <string>
+#include <vector>
+
+struct DeviceInfo {
+	std::string address;
+	std::string name;
+	int16_t rssi = 0;
+	std::map<uint16_t, std::vector<uint8_t>> manufacturerData;
+	std::vector<std::string> serviceUuids;
+};
+
+struct Characteristic {
+	std::string serviceUUid;
+	std::string characteristicUuid;
+};
+
+using DataCallback = std::function<void(const Characteristic& characteristic, const uint8_t* data, size_t length)>;
+using ConnectionCallback = std::function<void(bool connected, const std::string& address)>;
 
 class ITransport {
 public:
 	virtual ~ITransport() = default;
 
-	virtual bool open() = 0;
+	virtual bool startScan(int timeoutSeconds) = 0;
 
-	virtual bool close() = 0;
+	virtual void stopScan() = 0;
+	
+	virtual std::vector<DeviceInfo> getScanResults() const = 0;
 
-	virtual bool write(const uint8_t* data, size_t length) = 0;
+	virtual bool connect(const std::string& address) = 0;
+
+	virtual bool disconnect() = 0;
 
 	virtual bool isConnected() = 0;
 
-	virtual void setDataCallback(std::function<void(const uint8_t*, size_t)> callback) = 0;
+	virtual std::string getConnectedAddress() const = 0;
 
-	virtual void setConnectionCallback(std::function<void(bool)> callback) = 0;
+	virtual std::vector<std::string> getServices() const = 0;
+
+	virtual std::vector<Characteristic> getCharacteristics(const std::string& serviceUUid) const = 0;
+
+	virtual bool read(const Characteristic& characteristic, std::vector<uint8_t>& out) = 0;
+
+	virtual bool write(const Characteristic& characteristic, const uint8_t* data, size_t length, bool withResponse = true) = 0;
+
+	virtual bool subscribe(const Characteristic& characteristic, DataCallback callback) = 0;
+
+	virtual bool unsubscribe(const Characteristic& characteristic) = 0;
+
+	virtual size_t getMaxWriteSize() const = 0;
+
+	virtual void setConnectionCallback(ConnectionCallback callback) = 0;
 
 	virtual const char* getName() const = 0;
 };
