258626350a95322ae79bbe55e98a3f7e33259131
Aleksey <alekseybel2505@gmail.com>
2026-04-02 22:32:05 +0300

    little update in constants header

--- PrinterDriver/src/core/driver/protocol/Constants.h
+++ PrinterDriver/src/core/driver/protocol/Constants.h
@@ -8,13 +8,9 @@ namespace protocol {
     static constexpr const char* LWP3_BOOTLOADER_SERVICE_UUID = "00001625-1212-efde-1623-785feabcd123";
     static constexpr const char* LWP3_BOOTLOADER_CHAR_UUID = "00001626-1212-efde-1623-785feabcd123";
 
-    static constexpr const char* PYBRICKS_SERVICE_UUID = "c5f50001-8280-46da-89f4-6d8051e4aeef";
-    static constexpr const char* PYBRICKS_COMMAND_UUID = "c5f50002-8280-46da-89f4-6d8051e4aeef";
-    static constexpr const char* PYBRICKS_CAPABILITIES_UUID = "c5f50003-8280-46da-89f4-6d8051e4aeef";
-
-    static constexpr const char* NUS_SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
-    static constexpr const char* NUS_RX_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
-    static constexpr const char* NUS_TX_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
+    static constexpr const char* PRINTER_SERVICE_UUID = "c5f50001-8280-46da-89f4-6d8051e4aeef";
+    static constexpr const char* PRINTER_COMMAND_EVENT_UUID = "c5f50002-8280-46da-89f4-6d8051e4aeef";
+    static constexpr const char* PRINTER_CAPABILITIES_UUID = "c5f50003-8280-46da-89f4-6d8051e4aeef";
 
     static constexpr const char* LWP3_COMMAND_CHAR_UUID = "00001624-1212-efde-1623-785feabcd123";
 
