1c3d76834fbdd5876a4d1a308380849bdaf38995
Aleksey <alekseybel2505@gmail.com>
2026-04-05 22:46:26 +0300

    update constants in printer driver protocol

--- PrinterDriver/src/core/driver/protocol/Constants.h
+++ PrinterDriver/src/core/driver/protocol/Constants.h
@@ -5,14 +5,13 @@
 namespace protocol {
 
     static constexpr const char* LWP3_HUB_SERVICE_UUID = "00001623-1212-efde-1623-785feabcd123";
+    static constexpr const char* LWP3_HUB_CHARACTERISTIC_UUID = "00001624-1212-efde-1623-785feabcd123";
     static constexpr const char* LWP3_BOOTLOADER_SERVICE_UUID = "00001625-1212-efde-1623-785feabcd123";
     static constexpr const char* LWP3_BOOTLOADER_CHAR_UUID = "00001626-1212-efde-1623-785feabcd123";
 
-    static constexpr const char* PRINTER_SERVICE_UUID = "c5f50001-8280-46da-89f4-6d8051e4aeef";
-    static constexpr const char* PRINTER_COMMAND_EVENT_UUID = "c5f50002-8280-46da-89f4-6d8051e4aeef";
-    static constexpr const char* PRINTER_CAPABILITIES_UUID = "c5f50003-8280-46da-89f4-6d8051e4aeef";
-
-    static constexpr const char* LWP3_COMMAND_CHAR_UUID = "00001624-1212-efde-1623-785feabcd123";
+    static constexpr const char* PYBRICKS_SERVICE_UUID = "c5f50001-8280-46da-89f4-6d8051e4aeef";
+    static constexpr const char* PYBRICKS_COMMAND_EVENT_UUID = "c5f50002-8280-46da-89f4-6d8051e4aeef";
+    static constexpr const char* PYBRICKS_HUB_CAPABILITIES_UUID = "c5f50003-8280-46da-89f4-6d8051e4aeef";
 
     enum class PybricksCommand : uint8_t {
         StopUserProgram = 0x00,
@@ -25,11 +24,14 @@ namespace protocol {
     };
 
     enum class BootloaderCommand : uint8_t {
-        CheckSum = 0x11,
-        InitLoader = 0x44,
+        EraseFlash = 0x11,
         ProgramFlash = 0x22,
         StartApp = 0x33,
-        GetInfo = 0x55
+        InitLoader = 0x44,
+        GetInfo = 0x55,
+        GetCheckSum = 0x66,
+        GetFlashState = 0x77,
+        Disconnect = 0x88,
     };
 
 } // namespace protocol
