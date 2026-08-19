66f3b9dcf2407fb41daa91f2de1d4ef0e9ec5d4c
Aleksey <alekseybel2505@gmail.com>
2026-08-19 11:40:48 +0300

    added logging in techin hub program

--- LegoWirelessProtocol3/program/main.py
+++ LegoWirelessProtocol3/program/main.py
@@ -56,7 +56,7 @@ motor_y.control.limits(speed=1500, acceleration=3000)
 motor_x.reset_angle(0)
 motor_y.reset_angle(0)
 
-START_MARKER = b'\x01'
+# Команды протокола
 CMD_LINE = 0x01
 CMD_MOTION_BLOCK = 0x02
 
@@ -92,7 +92,7 @@ async def receiver():
 
         if state == WAIT_SYNC:
             # Ждём начало пакета: 0x01 (CMD_LINE) или 0x02 (CMD_MOTION_BLOCK)
-            if b == 0x01 or b == 0x02:
+            if b == CMD_LINE or b == CMD_MOTION_BLOCK:
                 packet_buf[0] = b
                 packet_idx = 1
                 expected_len = 0
@@ -109,13 +109,13 @@ async def receiver():
         packet_buf[packet_idx] = b
         packet_idx += 1
 
-        # После получения второго байта можем вычислить длину пакета
+        # После второго байта определяем длину пакета
         if packet_idx == 2:
-            if packet_buf[0] == 0x01:
-                # CMD_LINE: 0x01, axis, tx(4), ty(4), dur(2) = 12 байт
+            if packet_buf[0] == CMD_LINE:
+                # Формат: [0x01][axis][tx(4)][ty(4)][dur(2)] = 12 байт
                 expected_len = 12
-            elif packet_buf[0] == 0x02:
-                # CMD_MOTION_BLOCK: 0x02, count, count*(dx,dy,dur) = 2 + count*6
+            elif packet_buf[0] == CMD_MOTION_BLOCK:
+                # Формат: [0x02][count][count*(dx,dy,dur)] = 2 + count*6 байт
                 count = packet_buf[1]
                 if count == 0:
                     state = WAIT_SYNC
@@ -129,29 +129,26 @@ async def receiver():
                 expected_len = 0
                 continue
 
-        # Если пакет полностью собран
+        # Полный пакет?
         if expected_len > 0 and packet_idx == expected_len:
-            if packet_buf[0] == 0x01:
-                # Обработка одиночной линии
-                tx = struct.unpack('<i', packet_buf[2:6])[0]
-                ty = struct.unpack('<i', packet_buf[6:10])[0]
-                dur = struct.unpack('<H', packet_buf[10:12])[0]
+            if packet_buf[0] == CMD_LINE:
+                # Читаем без создания срезов
+                tx = struct.unpack_from('<i', packet_buf, 2)[0]
+                ty = struct.unpack_from('<i', packet_buf, 6)[0]
+                dur = struct.unpack_from('<H', packet_buf, 10)[0]
                 buffer_put(tx, ty, dur)
                 last_x = tx
                 last_y = ty
                 usys.stdout.buffer.write(b"LINE %d %d %d\n" % (tx, ty, dur))
                 usys.stdout.flush()
 
-            elif packet_buf[0] == 0x02:
-                # Обработка блока delta-сегментов
+            elif packet_buf[0] == CMD_MOTION_BLOCK:
                 count = packet_buf[1]
                 offset = 2
                 for _ in range(count):
-                    dx = struct.unpack('<h', packet_buf[offset:offset+2])[0]
-                    dy = struct.unpack('<h', packet_buf[offset+2:offset+4])[0]
-                    dur = struct.unpack('<H', packet_buf[offset+4:offset+6])[0]
+                    # Читаем сразу три поля одной операцией, без срезов
+                    dx, dy, dur = struct.unpack_from('<hhH', packet_buf, offset)
                     offset += 6
-
                     last_x += dx
                     last_y += dy
                     buffer_put(last_x, last_y, dur)
@@ -159,7 +156,8 @@ async def receiver():
             state = WAIT_SYNC
             packet_idx = 0
             expected_len = 0
-            await wait(5)
+            gc.collect()          # принудительная сборка мусора
+            await wait(5)         # даём поработать другим корутинам
 
 async def smooth_executor():
     usys.stdout.buffer.write(b"executor ready\n")
@@ -169,14 +167,27 @@ async def smooth_executor():
     current_y = 0
     segment_count = 0
     UPDATE_MS = 5
+    flow_stopped = False
 
+    # Ждём первоначального наполнения буфера
     while buffer_count() < 300:
         await wait(10)
 
     usys.stdout.buffer.write(b"start printing\n")
     usys.stdout.flush()
 
-    while True:        
+    while True:
+        # Flow control с гистерезисом
+        count = buffer_count()
+        if not flow_stopped and count > 3000:
+            usys.stdout.buffer.write(b"FLOW_STOP\n")
+            usys.stdout.flush()
+            flow_stopped = True
+        elif flow_stopped and count < 2000:
+            usys.stdout.buffer.write(b"FLOW_RESUME\n")
+            usys.stdout.flush()
+            flow_stopped = False
+
         seg = buffer_get()
         if seg is None:
             await wait(1)
@@ -219,6 +230,8 @@ async def main():
     try:
         await multitask(receiver(), smooth_executor())
     except Exception as e:
+        usys.stdout.buffer.write(b"CRITICAL_ERR: %s\n" % str(e).encode())
+        usys.stdout.flush()
         hub.light.on(Color.RED)
         while True:
             await wait(100)
