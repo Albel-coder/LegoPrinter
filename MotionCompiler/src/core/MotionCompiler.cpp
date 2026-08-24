75e4b0b1970cfdac2d9a34fceec7180457d691bf
Aleksey <alekseybel2505@gmail.com>
2026-08-24 22:09:14 +0300

    update motion compiler main logic

--- MotionCompiler/src/core/MotionCompiler.cpp
+++ MotionCompiler/src/core/MotionCompiler.cpp
@@ -1,19 +1,62 @@
 #include "MotionCompiler.h"
 
-MotionCompiler::MotionCompiler() {
+#include "../gcode/GCodeWriter.h"
+#include "../image/ImageProcessor.h"
+#include "../path/ContourOrganizer.h"
 
-}
+#include <fstream>
 
-MotionCompiler::~MotionCompiler() {
+MotionCompiler::MotionCompiler() = default;
 
-}
+MotionCompiler::~MotionCompiler() = default;
 
-bool MotionCompiler::compileImageProfiles(std::string inputFilename, std::string outputFilename) {
+bool MotionCompiler::compileImageProfiles(std::string inputFilename, std::string outputFilename, bool useSkeleton) {
+	ImageProcessor imageProcessor;
 
-	return true;
+	cv::Mat image;
+
+	if (!imageProcessor.loadImage(inputFilename, image)) {
+		return false;
+	}
+
+	cv::Mat binary = imageProcessor.toBinary(image);
+
+	if (binary.empty()) {
+		return false;
+	}
+
+	ImageProfile profile = imageProcessor.extractProfile(binary, useSkeleton);
+
+	if (profile.contours.empty()) {
+		return false;
+	}
+
+	ContourOrganizer organizer;
+
+	ImageProfile organized = organizer.organize(profile);
+
+	if (organized.contours.empty()) {
+		return false;
+	}
+
+	GCodeConfig config;
+	
+	// пока тестовые значения, позже они станут частью настроек компилятора
+	config.travelSpeedMmMin = 2000.0;
+	config.printSpeedMmMin = 500.0;
+	config.liftHeightMm = 5.0;
+	// это не принтерная калибровка а просто начальная визуальная конверсия
+	config.pixelsPerMm = 5.0;
+
+	GCodeWriter writer(config);
+
+	return writer.write(organized, outputFilename);
 }
 
 bool MotionCompiler::compileCode(std::string inputFilename, std::string outputFilename) {
 
-	return true;
+	(void)inputFilename;
+	(void)outputFilename;
+
+	return false;
 }
--- MotionCompiler/src/core/MotionCompiler.h
+++ MotionCompiler/src/core/MotionCompiler.h
@@ -1,14 +1,13 @@
 #pragma once
 
 #include <string>
-#include "../api/MotionCompilerAPI.h"
 
 class MotionCompiler {
 public:
-    explicit MotionCompiler();
+    MotionCompiler();
     ~MotionCompiler();
 
-    bool compileImageProfiles(std::string inputFilename, std::string outputFilename);
+    bool compileImageProfiles(std::string inputFilename, std::string outputFilename, bool useSkeleton);
 
     bool compileCode(std::string inputFilename, std::string outputFilename);
 };
